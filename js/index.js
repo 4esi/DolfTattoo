549406361d5d1eff83e5f012398a9ff6bfdbe3d5
4esi <zsoltnegyesi4@gmail.com>
2025-07-10 15:13:54 +0200

    Add minimal animation for all pages

--- js/index.js
+++ js/index.js
@@ -87,3 +87,44 @@ if(DomItems.backPicBtn) {
         
     })
 }
+
+//Animation
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 0) {
+    DomItems.nav.classList.add('navAnimOpen');
+    DomItems.nav.classList.remove('navAnimClose');
+} else {
+    DomItems.nav.classList.remove('navAnimOpen');
+    DomItems.nav.classList.add('navAnimClose');
+}
+})
+
+//
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('leftAnim');
+        } else {
+            entry.target.classList.remove('leftAnim'); // ha szeretnéd eltűntetni, amikor kimegy
+        }
+    })   
+});
+
+const sections = document.querySelectorAll('.sectionToObserve');
+sections.forEach(section => observer.observe(section));
+
+
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('opacityAnim');
+        } else {
+            entry.target.classList.remove('opacityAnim'); // ha szeretnéd eltűntetni, amikor kimegy
+        }
+    })   
+});
+
+const sections1 = document.querySelectorAll('.sectionToObserve1');
+sections1.forEach(section => observer1.observe(section));
